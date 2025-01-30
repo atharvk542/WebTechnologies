d680c128358f0aaf4e96462e7fed24680b9c8120
Atharv K <atharv.kanchi@gmail.com>
2025-01-30 14:45:42 -0600

    Finish JSEx2

--- JSEx2/script.js
+++ JSEx2/script.js
@@ -1,21 +1,29 @@
-function generateMadLib() {
-  const name = document.getElementById("name").value || "John";
-  const verb = document.getElementById("verb").value || "run";
-  const adjective = document.getElementById("adjective").value || "funny";
-
-  const story = `When ${name} gives a lecture, ${name} accepts that people ${verb} at their watches, but what ${name} does not ${verb} is when they look at it and raise it to their ${adjective} ear to find out if it has stopped working.`;
-
-  const storyDiv = document.getElementById("story");
-  storyDiv.innerHTML = story;
-}
-
 function calculateExpression() {
+  //Retrieves the expression
   const expression = document.getElementById("expression").value || "3 + 5";
+
+  //Tries to resolve expression, and catches error if expression is invalid
   try {
     const result = eval(expression).toFixed(2);
     document.getElementById("result").innerText = `${expression} = ${result}`;
   } catch (e) {
     alert("Invalid expression. Please try again.");
+  }
+}
+
+//Generates a madlib based on your name, a verb, and an adjective using prompts
+//Otherwise alerts if not all values are used
+function generateMadLib() {
+  let name = prompt("Enter your name:");
+  let verb = prompt("Enter a verb:");
+  let adjective = prompt("Enter an adjective:");
+
+  //Sets inner text of madlib page to specified story, otherwise generates alert
+  if (name && verb && adjective) {
+    let story = `${name} loves to ${verb} in the most ${adjective} way possible!`;
+    document.getElementById("story").innerText = story;
+  } else {
+    alert("Please enter all values to create a story!");
   }
 }
 
